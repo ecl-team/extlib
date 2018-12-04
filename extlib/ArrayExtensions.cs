a359600e75ff1c1d3d212eda30c517a49a1fe46c
BuildTools <unconfigured@null.spigotmc.org>
2018-12-04 22:47:56 +0100

    Update ArrayExtensions.cs

--- extlib/ArrayExtensions.cs
+++ extlib/ArrayExtensions.cs
@@ -8,8 +8,24 @@ namespace extlib
 {
     public static class ArrayExtensions
     {
+        /// <summary>
+        /// Combines all <see cref="System.Char"/> elements into a <see cref="System.String"/>
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static string AsString(this char[] array)
+        {
+            string a = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                a += array[i];
+            }
+            return a;
+        }
+
         public static T[] Resize<T>(this T[] array, int size)
         {
+            char[] e;
             Array.Resize(ref array, size);
             return array;
         }
