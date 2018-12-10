cc88a04dd90b482438f7b66c435d0e3720ee6c86
Eclipsum <wozzswe@gmail.com>
2018-12-10 18:20:02 +0100

    Update SmartStream.cs

--- extlib/Types/SmartStream.cs
+++ extlib/Types/SmartStream.cs
@@ -26,9 +26,44 @@ namespace extlib.Types
         public void Read(long slot1pos, long slot2pos, int length)
         {
             this.slot1pos = slot1pos;
-            this.slot2pos = slot2pos;
-            this.Slot1 = fs.ReadBytes(slot1pos, length);
-            this.Slot2 = fs.ReadBytes(slot2pos, length);
+            if (slot2pos - slot1pos < length)
+            {
+                this.slot2pos = slot1pos + length;
+            }
+            else
+                this.slot2pos = slot2pos;
+            this.Slot1 = fs.ReadBytes(this.slot1pos, length);
+            this.Slot2 = fs.ReadBytes(this.slot2pos, length);
+        }
+
+        public byte[] Get()
+        {
+            return this.Slot1;
+        }
+
+        public byte[] Get(Slot slot)
+        {
+            if (slot == Slot.First)
+                return this.Slot1;
+            if (slot == Slot.Second)
+                return this.Slot2;
+            return null;
+        }
+
+        public byte Get(long pos)
+        {
+            if (pos < slot2pos)
+                return Slot1[pos - slot1pos];
+            else
+                return Slot2[pos - slot2pos];
+        }
+
+        public void Set(long pos, byte Byte)
+        {
+            if (pos < slot2pos)
+                Slot1[pos - slot1pos] = Byte;
+            else
+                Slot2[pos - slot2pos] = Byte;
         }
 
         private long slot1pos, slot2pos;
